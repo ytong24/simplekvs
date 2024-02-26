1fded4eb882f5c2e9d2c33585685a4b9063e616e
Yan Tong <132377526+ytong24@users.noreply.github.com>
2024-02-25 21:39:54 -0800

    type conversion for generated proto struct

---
+++ src/pb.rs
@@ -0,0 +1,43 @@
+use crate::{command_request::RequestData, *};
+
+pub mod abi;
+
+impl CommandRequest {
+    /// create HSET
+    pub fn new_hset(table: impl Into<String>, key: impl Into<String>, value: Value) -> Self {
+        Self {
+            request_data: Some(RequestData::Hset(Hset {
+                table: table.into(),
+                pair: Some(Kvpair::new(key, value)),
+            })),
+        }
+    }
+}
+
+impl Kvpair {
+    /// create a new kv pair
+    pub fn new(key: impl Into<String>, value: Value) -> Self {
+        Self {
+            key: key.into(),
+            value: Some(value),
+        }
+    }
+}
+
+/// String to Value
+impl From<String> for Value {
+    fn from(s: String) -> Self {
+        Self {
+            value: Some(value::Value::String(s)),
+        }
+    }
+}
+
+/// &str to Value
+impl From<&str> for Value {
+    fn from(s: &str) -> Self {
+        Self {
+            value: Some(value::Value::String(s.into())),
+        }
+    }
+}
