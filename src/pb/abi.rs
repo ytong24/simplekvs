7e20ed5efb9607ad3ff932e76487c7fd0d76bf6b
Yan Tong <132377526+ytong24@users.noreply.github.com>
2024-02-25 20:52:37 -0800

    compile abi.proto

---
+++ src/pb/abi.rs
@@ -0,0 +1,161 @@
+#[allow(clippy::derive_partial_eq_without_eq)]
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct CommandRequest {
+    #[prost(oneof = "command_request::RequestData", tags = "1, 2, 3, 4, 5, 6, 7, 8, 9")]
+    pub request_data: ::core::option::Option<command_request::RequestData>,
+}
+/// Nested message and enum types in `CommandRequest`.
+pub mod command_request {
+    #[allow(clippy::derive_partial_eq_without_eq)]
+    #[derive(Clone, PartialEq, ::prost::Oneof)]
+    pub enum RequestData {
+        #[prost(message, tag = "1")]
+        Hget(super::Hget),
+        #[prost(message, tag = "2")]
+        Hgetall(super::Hgetall),
+        #[prost(message, tag = "3")]
+        Hmget(super::Hmget),
+        #[prost(message, tag = "4")]
+        Hset(super::Hset),
+        #[prost(message, tag = "5")]
+        Hmset(super::Hmset),
+        #[prost(message, tag = "6")]
+        Hdel(super::Hdel),
+        #[prost(message, tag = "7")]
+        Hmdel(super::Hmdel),
+        #[prost(message, tag = "8")]
+        Hexist(super::Hexist),
+        #[prost(message, tag = "9")]
+        Hmexist(super::Hmexist),
+    }
+}
+#[allow(clippy::derive_partial_eq_without_eq)]
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct CommandResponse {
+    /// status code: reuse HTTP 2xx/4xx/5xx
+    #[prost(uint32, tag = "1")]
+    pub status: u32,
+    /// error message
+    #[prost(string, tag = "2")]
+    pub message: ::prost::alloc::string::String,
+    /// return value
+    #[prost(message, repeated, tag = "3")]
+    pub values: ::prost::alloc::vec::Vec<Value>,
+    /// return kv pairs
+    #[prost(message, repeated, tag = "4")]
+    pub pairs: ::prost::alloc::vec::Vec<Kvpair>,
+}
+/// get the value from a table by key
+#[allow(clippy::derive_partial_eq_without_eq)]
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct Hget {
+    #[prost(string, tag = "1")]
+    pub table: ::prost::alloc::string::String,
+    #[prost(string, tag = "2")]
+    pub key: ::prost::alloc::string::String,
+}
+/// get all kv pairs in the table
+#[allow(clippy::derive_partial_eq_without_eq)]
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct Hgetall {
+    #[prost(string, tag = "1")]
+    pub table: ::prost::alloc::string::String,
+}
+/// get a set of values from a table by a set of keys
+#[allow(clippy::derive_partial_eq_without_eq)]
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct Hmget {
+    #[prost(string, tag = "1")]
+    pub table: ::prost::alloc::string::String,
+    #[prost(string, repeated, tag = "2")]
+    pub keys: ::prost::alloc::vec::Vec<::prost::alloc::string::String>,
+}
+/// type of value
+#[allow(clippy::derive_partial_eq_without_eq)]
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct Value {
+    #[prost(oneof = "value::Value", tags = "1, 2, 3, 4, 5")]
+    pub value: ::core::option::Option<value::Value>,
+}
+/// Nested message and enum types in `Value`.
+pub mod value {
+    #[allow(clippy::derive_partial_eq_without_eq)]
+    #[derive(Clone, PartialEq, ::prost::Oneof)]
+    pub enum Value {
+        #[prost(string, tag = "1")]
+        String(::prost::alloc::string::String),
+        #[prost(bytes, tag = "2")]
+        Binary(::prost::alloc::vec::Vec<u8>),
+        #[prost(int64, tag = "3")]
+        Integer(i64),
+        #[prost(double, tag = "4")]
+        Float(f64),
+        #[prost(bool, tag = "5")]
+        Bool(bool),
+    }
+}
+/// kv pair
+#[allow(clippy::derive_partial_eq_without_eq)]
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct Kvpair {
+    #[prost(string, tag = "1")]
+    pub key: ::prost::alloc::string::String,
+    #[prost(message, optional, tag = "2")]
+    pub value: ::core::option::Option<Value>,
+}
+/// insert a kv pair into the table,
+/// create the table if it doesn't exist
+#[allow(clippy::derive_partial_eq_without_eq)]
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct Hset {
+    #[prost(string, tag = "1")]
+    pub table: ::prost::alloc::string::String,
+    #[prost(message, optional, tag = "2")]
+    pub pair: ::core::option::Option<Kvpair>,
+}
+/// insert a set of kv pairs into the table,
+/// create the table if it doesn't exist
+#[allow(clippy::derive_partial_eq_without_eq)]
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct Hmset {
+    #[prost(string, tag = "1")]
+    pub table: ::prost::alloc::string::String,
+    #[prost(message, repeated, tag = "2")]
+    pub pairs: ::prost::alloc::vec::Vec<Kvpair>,
+}
+/// delete the key from the table, return the value
+#[allow(clippy::derive_partial_eq_without_eq)]
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct Hdel {
+    #[prost(string, tag = "1")]
+    pub table: ::prost::alloc::string::String,
+    #[prost(string, tag = "2")]
+    pub key: ::prost::alloc::string::String,
+}
+/// delete a set of keys from the table, return the values
+#[allow(clippy::derive_partial_eq_without_eq)]
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct Hmdel {
+    #[prost(string, tag = "1")]
+    pub table: ::prost::alloc::string::String,
+    #[prost(string, repeated, tag = "2")]
+    pub keys: ::prost::alloc::vec::Vec<::prost::alloc::string::String>,
+}
+/// check if key exist
+#[allow(clippy::derive_partial_eq_without_eq)]
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct Hexist {
+    #[prost(string, tag = "1")]
+    pub table: ::prost::alloc::string::String,
+    #[prost(string, tag = "2")]
+    pub key: ::prost::alloc::string::String,
+}
+/// check is a set of keys exist
+#[allow(clippy::derive_partial_eq_without_eq)]
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct Hmexist {
+    #[prost(string, tag = "1")]
+    pub table: ::prost::alloc::string::String,
+    #[prost(string, repeated, tag = "2")]
+    pub keys: ::prost::alloc::vec::Vec<::prost::alloc::string::String>,
+}
